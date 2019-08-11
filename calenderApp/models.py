1179721ad79338e26f0425b8f9971da1cfd05151
Litterial <yungice12@gmail.com>
2019-08-11 01:09:14 -0500

    ficing error

--- calenderApp/models.py
+++ calenderApp/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Appointment(models.Model):
-    name=models.CharField(max_length=35)
+    fname=models.CharField(max_length=35)
+    lname=models.CharField(max_length=35)
     address=models.CharField(max_length=50)
     email=models.EmailField()
     date=models.DateField()
+    # use 'choices' to limit the number of time slots that can be chosen
     time=models.TimeField(choices=(('07:00:00', '7 AM'),('08:00:00', '8 AM'),('09:00:00', '9 AM'),('10:00:00', '10 AM'),
         ('11:00:00', '11 AM'),('12:00:00', '12 PM'),('13:00:00', '1 AM'),('14:00:00', '2 AM'),('15:00:00', '3 AM'),('16:00:00', '4 AM'),
         ('17:00:00', '5 AM'),))
